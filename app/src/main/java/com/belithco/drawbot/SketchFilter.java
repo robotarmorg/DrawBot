2f5b8bc2b8cdd232ea5b38f93c63539d872bf450
javatechs <javatechs@gmail.com>
2016-09-16 10:53:35 -0700

    Filter to convert image to vectors.

---
+++ app/src/main/java/com/belithco/drawbot/SketchFilter.java
@@ -0,0 +1,14 @@
+package com.belithco.drawbot;
+
+import android.graphics.Bitmap;
+
+/**
+ * Filter to convert image to vectors.
+ */
+public class SketchFilter {
+    public Bitmap convert(Bitmap bitmap) {
+        Bitmap outBitmap = null;
+        //
+        return outBitmap;
+    }
+}
